a8fe7a6a81f9a20660bc12be88684954d577f7ea
Dzov <amelie.haladjian@gmail.com>
2017-01-25 14:27:39 +0100

    include php

--- experience.php
+++ experience.php
@@ -2,38 +2,12 @@
 <html>
 
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Amélie-Dzovinar Haladjian</title>
-    <link type="text/css" href="font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Nova+Round" rel="stylesheet">
-    <link type="text/css" href="./css/main.css" rel="stylesheet">
+    <?php include 'head.php' ; ?>
     <link type="text/css" href="./css/experience.css" rel="stylesheet">
   </head>
 
   <body>
-    <header class="flex">
-      <img src="img/octopus.svg" id="logo">
-      <nav class="flex">
-        <ul class="navList flex flexRow">
-          <li class="navList_Item">
-            <a href="index.html">Home</a>
-          </li>
-          <li class="navList_Item">
-            <a href="competences.html">Compétences</a>
-          </li>
-          <li class="navList_Item activePage">
-            <a href="experience.html">Expérience</a>
-          </li>
-          <li class="navList_Item">
-            <a href="formation.html">Formation</a>
-          </li>
-          <li class="navList_Item">
-            <a href="loisirs.html">Loisirs</a>
-          </li>
-        </ul>
-      </nav><!-- /nav -->
-    </header> <!-- /HEADER -->
+    <?php include "header.php" ?>
 
     <main>
       <section id="meemSection" class="flex">
@@ -94,23 +68,8 @@
       </section>
     </main>
 
-    <footer class="flex">
-          <p id="credit">Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
-          <ul class="footer_List flex">
-            <li class="footer_List_Item">
-              <a href="mailto:amelie.haladjian@gmail.com"><i class="fa fa-envelope"></i></a>
-            </li>
-            <li class="footer_List_Item">
-              <a href="http://facebook.com"><i class="fa fa-facebook"></i></a>
-            </li>
-            <li class="footer_List_Item">
-              <a href="http://twitter.com"><i class="fa fa-twitter"></i></a>
-            </li>
-            <li class="footer_List_Item">
-              <a href="http://linkedin.com"><i class="fa fa-linkedin"></i></a>
-            </li>
-          </ul>
-    </footer><!-- /Footer -->
+    <?php include "footer.php" ?>
+    <script src="./js/script.js" type="text/javascript" charset="utf-8"></script>
   </body>
 
 </html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <?php include 'head.php' ; ?>
+    <link type="text/css" href="./css/index.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <?php include 'header.php'; ?>
+
+    <main class="flex">
+      <article id="nameArticle">
+        <h2>Amélie-Dzovinar Haladjian</h2>
+        <h3>Développeuse Web</h3>
+      </article>
+    </main>
+
+    <?php include "footer.php" ?>
+    <script src="./js/script.js" type="text/javascript" charset="utf-8"></script>
+  </body>
+
+</html>
